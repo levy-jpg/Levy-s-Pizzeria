f7cd7d544fb2210d7f7379dccba485183becfce8
Levy Kariuki <levthigkari@gmail.com>
2020-03-02 10:15:17 +0300

    Created Function

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,16 @@ function Order(type, size, crust, topping) {
     this.crust = crust;
     this.topping = topping;
 }
+Order.prototype.fullOrder = function () {
+    return this.type + " with the topping of " + this.topping + " and " + this.crust + " as crust.";
+};
+
+function Total(price, quantity, delivery) {
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
+}
+
+Total.prototype.finalTotal = function () {
+    return this.price * this.quantity + this.delivery;
+};
